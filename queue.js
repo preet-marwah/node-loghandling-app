3dda3f0a825576d0be41213589079ed8d799087f
preet <45024648+preet-marwah@users.noreply.github.com>
2019-04-24 21:19:13 -0400

    Fixed Queue

--- queue.js
+++ queue.js
@@ -20,7 +20,75 @@
         this.first = null;
         this.last = null;
     }
+    //method to add new nodes to the (FIFO) Queue
+    add(value){
 
+        this.qLength++;
+
+        //as Queue is collection of nodes, create a new node
+        //also it's implementation is in FIFO so the new node is to be added at last node
+
+        const newNode = {
+            before:this.last,
+            after:null,
+            value:value,
+        }
+
+        //check if the Queue is empty and then set the new node as first node
+
+        if(this.last ===null){
+            this.first = newNode;
+        } else{ /* if the Queue is not empty then set the after pointer of the last node
+                    to the new node */
+        this.last.after = newNode;
+        }
+        // set the last pointer to the new node
+        this.last = newNode;
+
+    }
+
+    //method to remove nodes from the (FIFO) Queue
+
+    remove(){
+        /*as it's FIFO so just need to remove the first node and 
+        adjust the next node in the Queue to be the first node after 
+        the removal of the first node.*/
+
+        const firstNode = this.first;
+
+        //check if Queue is empty then return undefined
+
+        if (firstNode === null){
+            return undefined;
+        }
+
+        //make this.first point to next node
+        this.first = firstNode.after;
+
+        //check if the Queue only had one node
+        if(this.first === null){
+            this.last = null;
+        }
+
+        this.qLength--;
+
+        return firstNode.value;
+    }
+
+    toArray(){
+        const result = [];
+
+        let thisNode = this.first;
+        while (thisNode !== null){
+            result.push(thisNode.value);
+            thisNode = thisNode.after;
+        }
+
+        return result;
+    }
+
+
+    // method to return length of the queue
     length(){
         return this.qLength;
     }
